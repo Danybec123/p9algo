20fb8196af6a57483712dffc362479cda84a6b61
Danybec123 <120142909+Danybec123@users.noreply.github.com>
2023-04-20 16:17:47 -0600

    CAmbios
    Mucho texto

--- elpepe.js
+++ elpepe.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
   
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  var traer1=localStorage.getItem("Tag1")
+  var traer1=localStorage.getItem("Tag1:")
   var traer2=localStorage.getItem("sala:")
   function clickearse(){
     var inputd=document.getElementById("imnmnmput").value
@@ -20,3 +20,33 @@ var firebaseConfig = {
         likes:0
     })
   }
+  function clickaa(){
+    window.location=("etesech.html")
+  }
+  function tenermensajes(){
+    firebase.database().ref("/"+traer2).on('value',function (i){
+    i.forEach(function(r){
+      keya=r.key;
+      keyaya=r.val()
+      if (keya!="purpose") {
+        nombre=keyaya["nombre"]
+        mesaje=keyaya["mensaje"]
+        likes=keyaya["likes"]
+        h2="<h2>"+nombre+"</h2>"
+        h3="<h3>"+mesaje+"</h3>"
+        boton="<button onclick='onclick(this.id)' id="+keya+" value="+likes+">Likes"+likes+"</button>"  
+        document.getElementById("Div").innerHTML+=h2+h3+boton  
+
+      }
+    });
+    })
+  }
+  function onclick(l){
+     var variable1 =l;
+     var valor=document.getElementById(variable1).value
+     var actualizar=valor+1
+     firebase.database().ref(traer2).child(keya).update({
+      likes:actualizar
+     })
+  }
+  tenermensajes()
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var usuario=document.getElementById("nombre").value
+
 function presiona(){
+    var usuario=document.getElementById("nombre").value
     localStorage.setItem("Tag1:",usuario)
     window.location="etesech.html"
+
 }
